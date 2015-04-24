bacf1ffddac796f2f388317a15ad7f14d560a5d6
Stebbbi <stebbbi97@gmail.com>
2015-04-24 00:40:15 +0000

    Save System and Auto Increment

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-var greys = 0;
-var lblues = 0;
-var blues = 0;
-var purples = 0;
-var pinks = 0;
-var reds = 0;
+var player = {
+    greys: 0,
+    lblues: 0,
+    blues: 0,
+    purples: 0,
+    pinks: 0,
+    reds: 0,
+    money: 0,
+    keys: 0,
+    idleAccounts: 0,
+    SellBots: 0,
+    inc: 0,
+    click: 1,
+    iAccCost: 50,
+    SellBotCost: 100
+}
 
 var greyPrice = 0.1;
 var lbluePrice = 0.3;
@@ -12,19 +22,18 @@ var purplePrice = 1;
 var pinkPrice = 5;
 var redPrice = 10;
 
-var money = 0;
+var keyCost = 2.49;
 
 var timeoutID;
 var hide;
 
-var keys = 0;
-var idleAccounts = 0;
+var inc = 0;
 
 var Grey = document.getElementById("greys");
 var LBlue = document.getElementById("lblues");
 var Blue = document.getElementById("blues");
 var Purple = document.getElementById("purples");
-var Pink = document.getElementById("pinks");
+var Pink = document.getElementById("pinks");    
 var Red = document.getElementById("reds");
 var Money = document.getElementById("moneys");
 
@@ -35,53 +44,51 @@ var PurpleSell = document.getElementById("purplesSell");
 var PinkSell = document.getElementById("pinksSell");
 var RedSell = document.getElementById("redsSell");
 
-items = {
-    "Key": ["Key", "2.49"],
-    "idleAccount": ["IdleAccount", "50"],
-    "SellBot": ["SellBot", "100"]
-};
-
 function display() {
     "use strict";
-    Grey.firstChild.data = "Comsumer: " + greys;
-    LBlue.firstChild.data = "Industrial: " + lblues;
-    Blue.firstChild.data = "Mil-Spec: " + blues;
-    Purple.firstChild.data = "Restricted: " + purples;
-    Pink.firstChild.data = "Classified: " + pinks;
-    Red.firstChild.data = "Covert: " + reds;
-    var rounded = Math.round(money * 10) / 10;
+    Grey.firstChild.data = "Comsumer: " + player.greys;
+    LBlue.firstChild.data = "Industrial: " + player.lblues;
+    Blue.firstChild.data = "Mil-Spec: " + player.blues;
+    Purple.firstChild.data = "Restricted: " + player.purples;
+    Pink.firstChild.data = "Classified: " + player.pinks;
+    Red.firstChild.data = "Covert: " + player.reds;
+    var rounded = Math.round(player.money * 10) / 10;
     var fixed = rounded.toFixed(1);
-    parseFloat(money.toFixed(2));
+    parseFloat(player.money.toFixed(2));
     Money.firstChild.data = "Money: " + fixed;
     
-    GreySell.firstChild.data = "Comsumer: " + greys;
-    LBlueSell.firstChild.data = "Industrial: " + lblues;
-    BlueSell.firstChild.data = "Mil-Spec: " + blues;
-    PurpleSell.firstChild.data = "Restricted: " + purples;
-    PinkSell.firstChild.data = "Classified: " + pinks;
-    RedSell.firstChild.data = "Covert: " + reds;
+    GreySell.firstChild.data = "Comsumer: " + player.greys;
+    LBlueSell.firstChild.data = "Industrial: " + player.lblues;
+    BlueSell.firstChild.data = "Mil-Spec: " + player.blues;
+    PurpleSell.firstChild.data = "Restricted: " + player.purples;
+    PinkSell.firstChild.data = "Classified: " + player.pinks;
+    RedSell.firstChild.data = "Covert: " + player.reds;
+    
+    document.getElementById("keys").firstChild.data = "Keys: " + keys;
+    document.getElementById("iAcc").firstChild.data = "Idle Accounts: " + player.idleAccounts;
+    document.getElementById("sellbot").firstChild.data = "SellBots: " + player.SellBots;
 }
 
 function idle() {
     "use strict";
     var ran = (Math.round(Math.random() * 1000)) + 1;
     if (ran === 1) {
-        reds = reds + 1;
+        player.reds = player.reds + 1;
         display();
     } else if (ran > 1 && ran < 5) {
-        pinks = pinks + 1;
+        player.pinks = player.pinks + 1;
         display();
     } else if (ran > 5 && ran < 15) {
-        purples = purples + 1;
+        player.purples = player.purples + 1;
         display();
     } else if (ran > 15 && ran < 50) {
-        blues = blues + 1;
+        player.blues = player.blues + 1;
         display();
     } else if (ran > 50 && ran < 100) {
-        lblues = lblues + 1;
+        player.lblues = player.lblues + 1;
         display();
     } else if (ran > 100 && ran < 900) {
-        greys = greys + 1;
+       player.greys = player.greys + 1;
         display();
     }
 }
@@ -98,6 +105,30 @@ function showWarning(x) {
     //timeoutID = setTimeout(hideWarning, 3000);
 }
 
+function set_cookie(cookie_name,value) {
+    expiry = new Date();   
+    expiry.setTime(new Date().getTime() + (10 * 60 * 1000)); 
+    var c_value = escape(btoa(JSON.stringify(value))) + 
+    "; expires=" + expiry.toUTCString();
+    document.cookie=cookie_name + "=" + c_value;
+}
+
+function get_cookie(cookie_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + cookie_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(cookie_name + "=");
+    }
+    if (c_start == -1) return false;
+    c_start = c_value.indexOf("=", c_start) + 1;
+    var c_end = c_value.indexOf(";", c_start);
+    if (c_end == -1) {
+        c_end = c_value.length;
+    }
+    c_value = atob(unescape(c_value.substring(c_start,c_end)));
+    return JSON.parse(c_value);
+}
+
 function sell(x) {
     "use strict";
     switch (x) {
@@ -105,8 +136,8 @@ function sell(x) {
         if (greys === 0) {
             showWarning('sellWarning');
         } else {
-            money = money + greyPrice;
-            greys = greys - 1;
+            player.money = player.money + greyPrice;
+           player.greys = player.greys - 1;
             display();
         }
         break;
@@ -114,8 +145,8 @@ function sell(x) {
         if (lblues === 0) {
             showWarning('sellWarning');
         } else {
-            money = money + lbluePrice;
-            lblues = lblues - 1;
+            player.money = player.money + lbluePrice;
+            player.lblues = player.lblues - 1;
             display();
         }
         break;
@@ -123,8 +154,8 @@ function sell(x) {
         if (blues === 0) {
             showWarning('sellWarning');
         } else {
-            money = money + bluePrice;
-            blues = blues - 1;
+            player.money = player.money + bluePrice;
+            player.blues = player.blues - 1;
             display();
         }
         break;
@@ -132,8 +163,8 @@ function sell(x) {
         if (purples === 0) {
             showWarning('sellWarning');
         } else {
-            money = money + purplePrice;
-            purples = purples - 1;
+            player.money = player.money + purplePrice;
+            player.purples = player.purples - 1;
             display();
         }
         break;
@@ -141,8 +172,8 @@ function sell(x) {
         if (pinks === 0) {
             showWarning('sellWarning');
         } else {
-            money = money + pinkPrice;
-            pinks = pinks - 1;
+            player.money = player.money + pinkPrice;
+            player.pinks = player.pinks - 1;
             display();
         }
         break;
@@ -150,8 +181,8 @@ function sell(x) {
         if (reds === 0) {
             showWarning('sellWarning');
         } else {
-            money = money + redPrice;
-            reds = reds - 1;
+            player.money = player.money + redPrice;
+            player.reds = player.reds - 1;
             display();
         }
         break;
@@ -168,8 +199,8 @@ function tradeup(x) {
         if (greys < 10) {
             showWarning('tradeupWarning');
         } else {
-            greys = greys - 10;
-            lblues = lblues + 1;
+           player.greys = player.greys - 10;
+            player.lblues = player.lblues + 1;
             display();
         }
         break;
@@ -177,8 +208,8 @@ function tradeup(x) {
         if (lblues < 10) {
             showWarning('tradeupWarning');
         } else {
-            lblues = lblues - 10;
-            blues = blues + 1;
+            player.lblues = player.lblues - 10;
+            player.blues = player.blues + 1;
             display();
         }
         break;
@@ -186,8 +217,8 @@ function tradeup(x) {
         if (blues < 10) {
             showWarning('tradeupWarning');
         } else {
-            blues = blues - 10;
-            purples = purples + 1;
+            player.blues = player.blues - 10;
+            player.purples = player.purples + 1;
             display();
         }
         break;
@@ -195,8 +226,8 @@ function tradeup(x) {
         if (purples < 10) {
             showWarning('tradeupWarning');
         } else {
-            purples = purples - 10;
-            pinks = pinks + 1;
+            player.purples = player.purples - 10;
+            player.pinks = player.pinks + 1;
             display();
         }
         break;
@@ -204,8 +235,8 @@ function tradeup(x) {
         if (pinks < 10) {
             showWarning('tradeupWarning');
         } else {
-            pinks = pinks - 10;
-            reds = reds + 1;
+            player.pinks = player.pinks - 10;
+            player.reds = player.reds + 1;
             display();
         }
         break;
@@ -218,32 +249,33 @@ function tradeup(x) {
 function buy(x) {
     switch (x) {
     case 'key':
-        if (money - 2.49 < 0) {
+        if (player.money - keyCost < 0) {
             showWarning('buyWarning');
             break;
         } else {
             keys = keys + 1;
-            money = money - items.Key[1];
+            player.money = player.money - keyCost;
             display();
             break;
         }
     case 'idleAccount':
-        if (money - items.idleAccount[1] < 0) {
+        if (player.money - player.iAccCost < 0) {
             showWarning('buyWarning');
             break;
         } else {
-            idleAccounts = idleAccounts + 1;
-            money = money - items.idleAccount[1];
+            player.inc = player.inc + 0.5;
+            player.idleAccounts += 1;
+            player.money = player.money - player.iAccCost;
             display();
             break;
         }
     case 'SellBot':
-        if (money - items.SellBot[1] < 0) {
+        if (player.money - player.SellBotCost < 0) {
             showWarning('buyWarning');
             break;
         } else {
-            idleAccounts = idleAccounts + 1;
-            money = money - items.SellBot[1];
+            player.SellBots += 1;
+            player.money = player.money - player.SellBotCost;
             display();
             break;
         }
@@ -252,10 +284,38 @@ function buy(x) {
     
 }
 
+function load_game() {
+    var save_data = get_cookie('IdleCSGO_save');
+    console.log(save_data);
+    if (!save_data) return;
+    console.log(save_data);
+    player = save_data;
+    display();
+}
+function save_game() {
+    set_cookie('IdleCSGO_save', player);
+    showWarning('saveInfo');
+}
+
 function showDiv(show) {
     "use strict";
     $('#' + hide).hide();
     $('#' + show).show();
     hide = show;
 }
-            
+
+function autoInc() {
+    "use strict";
+    for (var f = 0; f < player.inc; f++) {
+        idle();
+    }
+    if (player.inc % 1 === 0) {
+        setTimeout(autoInc, 1000);
+    } else {
+        setTimeout(autoInc, 2000);
+    }
+}
+
+load_game();
+autoInc();
+setInterval(function () { save_game(); }, 10000);           
